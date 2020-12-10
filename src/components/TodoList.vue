4426b3d11c791c5ebdee358d608051960a3d994b
19975273152 <13656426992@163.com>
2020-12-10 19:49:09 +0800

    1

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="TodoList">
     <div class="header">
-      <span>ToDoList</span>
-      <el-input v-model.trim="addTodo" placeholder="添加ToDo"></el-input>
+      <span class="headerName">ToDoList</span>
+      <el-input v-model="addTodo" placeholder="添加ToDo" @keyup.enter.native="handleAdd(addTodo)"></el-input>
     </div>
     <div class="body">
       <h2>正在进行</h2>
-      <div v-for="(todo, index) in todoList" :key="index" class="doing">
+      <div v-for="(todo, index) in getTodoList" :key="index" class="doing">
         <el-checkbox></el-checkbox>
         <el-input v-if="todo.edit"></el-input>
         <span v-else>{{todo.name}}</span>
+        <i class="el-icon-delete" @click="handleDelete(todo)"></i>
       </div>
     </div>
     <div class="body">
       <h2>已经完成</h2>
-       <div v-for="(todo, index) in todoList" :key="index" class="doing">
+       <div v-for="(done, index) in getDoneList" :key="index" class="doing">
         <el-checkbox></el-checkbox>
-        <span >{{todo.name}}</span>
+        <span >{{done.name}}</span>
+        <i class="el-icon-delete" @click="handleDelete(done)"></i>
       </div>
     </div>
   </div>
@@ -29,11 +31,32 @@ export default {
     return {
       addTodo: '',
       todoList: [
-        {name: '待办事项1', status: 'done',edit: false},
-        {name: '待办事项1', status: 'done',edit: false},
-        {name: '待办事项1', status: 'todo', edit: false},
-        {name: '待办事项1', status: 'todo',edit: false},
+        { name: '待办事项1', status: 'done',edit: false},
+        { name: '待办事项2', status: 'done',edit: false},
+        { name: '待办事项3', status: 'todo', edit: false},
+        { name: '待办事项4', status: 'todo',edit: false},
       ]
+    }
+  },
+  computed:{
+    getDoneList() {
+      return this.todoList.filter(val => val.status === 'done')
+    },
+    getTodoList() {
+      return this.todoList.filter(val => val.status === 'todo')
+    },
+    getTodoId(){
+      return this.todoList.map(val => val.id = )
+    }
+  },
+  methods:{
+    handleAdd(item) {
+      this.todoList.push({name: item, status: 'todo', edit: false})
+      this.addTodo = ''
+    },
+    handleDelete(item){
+      console.log('item====', this.todoList.filter(val => val.id === item.id))
+      // this.todoList.splice(index,1)
     }
   }
 }
@@ -51,6 +74,7 @@ export default {
   height: 60px;
   line-height: 60px;
   text-align: center;
+  font-size: bold;
   background: rgb(48, 42, 42);
   & .el-input ::v-deep .el-input__inner {
     width: 50%;
@@ -58,5 +82,11 @@ export default {
 }
 .doing{
   display: flex;
+}
+.el-icon-delete{
+  margin-left: 15px;
+}
+.headerName{
+  margin-left:300px;
 }
 </style>
