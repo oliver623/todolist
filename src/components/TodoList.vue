06cf830d5a4c6167ab2c34ef22bca6feb2503035
19975273152 <13656426992@163.com>
2020-12-11 14:53:11 +0800

    tj

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -12,8 +12,14 @@
       <h2>正在进行</h2>
       <div v-for="(todo, index) in getTodoList" :key="index" class="doing">
         <el-checkbox v-model="todo.status" true-label="done" false-label="todo"></el-checkbox>
-        <el-input v-if="todo.edit" v-model="todo.name" ref="editTodo" @blur="handleEdit(todo)" @keyup.enter.native="handleEdit(todo)"></el-input>
-        <span v-else @dblclick="handleOpenEdit(todo)">{{todo.name}}</span>
+        <el-input
+          v-if="todo.edit"
+          v-model="todo.name"
+          ref="editTodo"
+          @blur="handleEdit(todo)"
+          @keyup.enter.native="handleEdit(todo)"
+        ></el-input>
+        <span v-else class="showName" @dblclick="handleOpenEdit(todo)">{{todo.name}}</span>
         <i class="el-icon-delete" @click="handleDelete(todo)"></i>
       </div>
     </div>
@@ -21,7 +27,7 @@
       <h2>已经完成</h2>
       <div v-for="(done, index) in getDoneList" :key="index" class="doing">
         <el-checkbox checked disabled></el-checkbox>
-        <span>{{done.name}}</span>
+        <span class="showName">{{done.name}}</span>
         <i class="el-icon-delete" @click="handleDelete(done)"></i>
       </div>
     </div>
@@ -33,25 +39,29 @@ export default {
   name: 'TodoList',
   data() {
     return {
-      addTodo: '',
+      addTodo: '', // 新增的待办事项的名称
       todoList: [
         { id: 0, name: '待办事项1', status: 'done', edit: false },
         { id: 1, name: '待办事项2', status: 'done', edit: false },
         { id: 2, name: '待办事项3', status: 'todo', edit: false },
         { id: 3, name: '待办事项4', status: 'todo', edit: false }
-      ],
+      ], // 默认待办事项
       index: 4
     }
   },
   computed: {
+    // 计算已完成的事项
     getDoneList() {
       return this.todoList.filter(val => val.status === 'done')
     },
+
+    // 计算待办事项
     getTodoList() {
       return this.todoList.filter(val => val.status === 'todo')
     }
   },
   methods: {
+    // 新增待办事项
     handleAdd(item) {
       if (item == '')
         return this.$message({
@@ -59,17 +69,17 @@ export default {
           type: 'warning'
         })
       this.todoList.push({ id: this.index++, name: item, status: 'todo', edit: false })
-
       this.addTodo = ''
     },
-    handleDelete(index, item) {
-      // let idx = this.todoList.indexOf(item)
-      let idx = this.todoList.findIndex(val => val === item)
+
+    // 删除待办事项
+    handleDelete(item) {
+      // let idx = this.todoList.indexOf(item) //得到当前事项的index  方法1
+      let idx = this.todoList.findIndex(val => val === item)  //得到当前事项的index  方法2
       this.todoList.splice(idx, 1)
     },
-    changeStatus(item) {
-      item.status = 'done'
-    },
+    
+    // 编辑待办事项
     handleEdit(item) {
       if (item.name == '')
         return this.$message({
@@ -78,13 +88,14 @@ export default {
         })
       item.edit = false
     },
-    handleOpenEdit(item){
+
+    // 双击打开编辑状态
+    handleOpenEdit(item) {
       item.edit = true
       this.$nextTick(() => {
         this.$refs.editTodo[0].focus()
-        console.log('this.$refs.editTodo====', this.$refs.editTodo)
       })
-}
+    }
   }
 }
 </script>
@@ -115,5 +126,8 @@ export default {
 }
 .headerName {
   margin-left: 300px;
+}
+.showName{
+  margin: 0 15px;
 }
 </style>
